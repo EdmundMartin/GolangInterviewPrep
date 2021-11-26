6549b7ec569e06e04f3ee0c3014003c5646f0026
EdmundMartin <edmartin101@gmail.com>
2021-11-26 19:35:05 +0000

    Additional easy algo

---
+++ algoexpert/easy/remove_duplicates_from_ordered_linked_list.go
@@ -0,0 +1,42 @@
+package easy
+
+type LinkedList struct {
+	Value int
+	Next *LinkedList
+}
+
+
+func RemoveDuplicatesFromLinkedList(head *LinkedList) *LinkedList {
+	var prev *LinkedList = nil
+	currentNode := head
+	for currentNode != nil {
+		if prev != nil && prev.Value != currentNode.Value {
+			prev.Next = currentNode
+			prev = currentNode
+		}
+		if prev != nil && currentNode.Next == nil {
+			if currentNode.Value == prev.Value {
+				prev.Next = nil
+			}
+		}
+		if prev == nil {
+			prev = currentNode
+		}
+		currentNode = currentNode.Next
+	}
+	return head
+}
+
+func RemoveDuplicatesFromLinkedListAlt(head *LinkedList) *LinkedList {
+	currentNode := head
+	for currentNode != nil {
+		nextNode := currentNode.Next
+		for nextNode != nil && nextNode.Value == currentNode.Value {
+			nextNode = nextNode.Next
+		}
+
+		currentNode.Next = nextNode
+		currentNode = nextNode
+	}
+	return head
+}
---
+++ algoexpert/easy/remove_duplicates_from_ordered_linked_list_test.go
@@ -0,0 +1,47 @@
+package easy
+
+import (
+	"reflect"
+	"testing"
+)
+
+func SliceToLinkedList(values []int) *LinkedList {
+	dummyHead := &LinkedList{Value: 0}
+	root := dummyHead
+	for _, val := range values {
+		root.Next = &LinkedList{Value: val}
+		root = root.Next
+	}
+	return dummyHead.Next
+}
+
+
+func LinkedListToSlice(head *LinkedList) []int {
+	var values []int
+	for head != nil {
+		values = append(values, head.Value)
+		head = head.Next
+	}
+	return values
+}
+
+
+func TestRemoveDuplicatesFromLinkedList(t *testing.T) {
+	testList := SliceToLinkedList([]int{1, 2, 3, 4, 4, 5, 5, 6, 6, 8, 9})
+	expectedList := []int{1, 2, 3, 4, 5, 6, 8, 9}
+	result := RemoveDuplicatesFromLinkedList(testList)
+
+	if !reflect.DeepEqual(expectedList, LinkedListToSlice(result)) {
+		t.Errorf("Expected matching results, got: %v and %v", expectedList, result)
+	}
+}
+
+func TestRemoveDuplicatesFromLinkedListAlt(t *testing.T) {
+	testList := SliceToLinkedList([]int{1, 2, 3, 4, 4, 5, 5, 6, 6, 8, 9})
+	expectedList := []int{1, 2, 3, 4, 5, 6, 8, 9}
+	result := RemoveDuplicatesFromLinkedListAlt(testList)
+
+	if !reflect.DeepEqual(expectedList, LinkedListToSlice(result)) {
+		t.Errorf("Expected matching results, got: %v and %v", expectedList, result)
+	}
+}
