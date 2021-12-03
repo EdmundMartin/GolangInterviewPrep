5535d8ba3dff4a7c9816622cc3416a0fd18fa058
EdmundMartin <edmartin101@gmail.com>
2021-12-03 20:25:55 +0000

    Another easy

---
+++ algoexpert/easy/depth_first_search.go
@@ -0,0 +1,23 @@
+package easy
+
+type NodeDepthFirst struct {
+	Name string
+	Children []*NodeDepthFirst
+}
+
+func (n *NodeDepthFirst) AddChild(name string) *NodeDepthFirst {
+	n.Children = append(n.Children, &NodeDepthFirst{
+		Name:     name,
+		Children: []*NodeDepthFirst{},
+	})
+	return n
+}
+
+
+func (n *NodeDepthFirst) DepthFirstSearch(array *[]string) *[]string {
+	*array = append(*array, n.Name)
+	for _, child := range n.Children {
+		child.DepthFirstSearch(array)
+	}
+	return array
+}
